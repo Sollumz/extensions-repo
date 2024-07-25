d3eadf8dd2b5dd27e77eee8441638469a653f45a
alexguirre <alexguirre.aa@gmail.com>
2024-07-25 20:06:57 +0200

    tweak: generate a valid example JSON

--- gen_listing.py
+++ gen_listing.py
@@ -9,6 +9,45 @@ Example:
 import argparse
 from pathlib import Path
 
+EXAMPLE_LISTING_JSON = """{
+   "version": "v1",
+   "blocklist": [],
+   "data": [
+    {
+       "id": "sollumz",
+       "name": "Sollumz",
+       "tagline": "Pretty epic add-on",
+       "version": "2.5.0",
+       "type": "add-on",
+       "archive_size": 856650,
+       "archive_hash": "sha256:3d2972a6f6482e3c502273434ca53eec0c5ab3dae628b55c101c95a4bc4e15b2",
+       "archive_url": "https://repo.sollumz.org/packages/sollumz",
+       "blender_version_min": "4.2.0",
+       "maintainer": "Sollumz Team",
+       "tags": ["Pipeline"],
+       "license": ["SPDX:MIT"],
+       "website": "http://sollumz.org/",
+       "schema_version": "1.0.0"
+    },
+    {
+       "id": "sollumz-nightly",
+       "name": "Sollumz (Nightly)",
+       "tagline": "Pretty epic add-on. Latest. Unstable",
+       "version": "2.5.0-nightly-abcd123",
+       "type": "add-on",
+       "archive_size": 856650,
+       "archive_hash": "sha256:3d2972a6f6482e3c502273434ca53eec0c5ab3dae628b55c101c95a4bc4e15b2",
+       "archive_url": "https://repo.sollumz.org/packages/sollumz-nightly",
+       "blender_version_min": "4.2.0",
+       "maintainer": "Sollumz Team",
+       "tags": ["Pipeline"],
+       "license": ["SPDX:MIT"],
+       "website": "http://sollumz.org/",
+       "schema_version": "1.0.0"
+    },
+    ]
+}
+"""
 
 def main():
     parser = argparse.ArgumentParser(
@@ -31,7 +70,7 @@ def main():
     output_directory.mkdir(exist_ok=True)
 
     with open(output_directory.joinpath("index.json"), mode="w") as f:
-        f.write('{"hello": "world"}')
+        f.write(EXAMPLE_LISTING_JSON)
 
 if __name__ == "__main__":
     main()
